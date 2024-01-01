0a5f4ec92cad0dac048ef93084b8b44a21179366
Denis Zhmurenko <denis2.zhmurenko@live.uwe.ac.uk>
2024-01-01 18:50:08 +0000

    Manage promocodes

---
+++ frames/manage_promocodes/manage_promocodes.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+from tkinter import messagebox
+from components.table import TableComponent
+from frames.manage_promocodes.popups.add_edit_promocode import AddEditPromocodePopup
+
+class ManagePromocodesFrame(tk.Frame):
+    def __init__(self, parent, app):
+        tk.Frame.__init__(self, parent)
+        self.app = app
+
+        self.grid_columnconfigure(tuple(range(2)), weight=1)
+        tk.Label(self, text="Promocodes", font=self.app.title_font).grid(row=0, column=0, sticky="w")
+        tk.Button(self, text="Add Promocode", command=self.add_promocode, font=self.app.base_font, bg="blue", fg="white").grid(row=0, column=1, sticky='e')
+
+        self.table = TableComponent(
+            self,
+            headings=['Promocodes', 'Discount', 'Valid until', 'Actions'],
+            data=self.app.hrms.promocodes,
+            get_row=self.get_row
+        )
+        self.table.grid(row=1, column=0, columnspan=2, sticky='ew')
+    
+    def get_row(self, table, row, promocode):
+        tk.Label(table, text=promocode.id, anchor='w', font=self.app.base_font).grid(row=row, column=0, sticky='ew')
+        tk.Label(table, text=f'{promocode.discount}%', anchor='w', font=self.app.base_font).grid(row=row, column=1, sticky='ew')
+        tk.Label(table, text=promocode.valid_till.strftime('%d.%m.%Y'), anchor='w', font=self.app.base_font).grid(row=row, column=2, sticky='ew')
+        actions_frame = tk.Frame(table)
+        actions_frame.grid(row=row, column=3, sticky='ew')
+        actions_frame.grid_columnconfigure(tuple(range(2)), weight=1)
+        tk.Button(actions_frame, text='✎', command=lambda promocode=promocode: self.edit_promocode(promocode)).grid(row=0, column=0, sticky='ew')
+        tk.Button(actions_frame, text='✖', command=lambda promocode=promocode: self.delete_promocode(promocode)).grid(row=0, column=1, sticky='ew')
+
+    def refresh(self):
+        self.table.update_data(self.app.hrms.promocodes)
+
+    def add_promocode(self):
+        popup = AddEditPromocodePopup(self)
+        self.wait_window(popup)
+
+    def edit_promocode(self, promocode):
+        popup = AddEditPromocodePopup(self, promocode)
+        self.wait_window(popup)
+
+    def delete_promocode(self, promocode):
+        action = messagebox.askquestion('Delete promocode', f'Are you sure you want to delete "{promocode.id}"?', icon='warning')
+        if action == 'yes':
+            self.app.hrms.delete_promocode(promocode)
+            self.refresh()
--- orm/base.py
+++ orm/base.py
@@ -5,14 +5,18 @@ from orm.user import User
 from orm.entities.user import UserEntity
 from orm.restaurant import Restaurant
 from orm.entities.restaurant import RestaurantEntity
+from orm.promocode import Promocode
+from orm.entities.promocode import Promocode as PromocodeEntity
 
 class HRMS:
     users: list[User] = []
     restaurants: list[Restaurant] = []
+    promocodes: list[Promocode] = []
 
     def __init__(self):
         self.users = [User(user_entity=user_entity) for user_entity in session.scalars(select(UserEntity))]
         self.restaurants = [Restaurant(restaurant_entity=restaurant_entity) for restaurant_entity in session.scalars(select(RestaurantEntity))]
+        self.promocodes = [Promocode(promocode_entity=promocode_entity) for promocode_entity in session.scalars(select(PromocodeEntity))]
 
     def find_user(self, phone):
         return next(user for user in self.users if user.phone == phone)
@@ -22,7 +26,6 @@ class HRMS:
         
     def delete_user(self, user: User):
         user.delete()
-        session.commit()
         self.users.remove(user)
 
     def add_restaurant(self, restaurant: Restaurant):
@@ -30,5 +33,14 @@ class HRMS:
 
     def delete_restaurant(self, restaurant: Restaurant):
         restaurant.delete()
-        session.commit()
         self.restaurants.remove(restaurant)
+
+    def find_promocode(self, id):
+        return next(promocode for promocode in self.promocodes if promocode.id == id)
+    
+    def add_promocode(self, promocode: Promocode):
+        self.promocodes.append(promocode)
+
+    def delete_promocode(self, promocode: Promocode):
+        promocode.delete()
+        self.promocodes.remove(promocode)
